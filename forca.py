89c734c656c319fd872c67ec4190b69be396ae31
Anderson-olv <52669812+Anderson-olv@users.noreply.github.com>
2023-11-15 15:34:28 -0300

    Adicionando projeto jogos no git

---
+++ forca.py
@@ -0,0 +1,17 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    palavra_secreta = "banana"
+
+    acertou = False
+    enforcou = False
+
+    while (not acertou and not enforcou):
+        print('Continue Jogando.')
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
